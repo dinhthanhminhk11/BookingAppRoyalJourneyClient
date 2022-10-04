772b00e810ab839d288d9418fdf9e3c5b3fc83b9
LongDuyDz <93507947+LongDuyDz@users.noreply.github.com>
2022-10-04 21:07:34 +0700

    SplashScreen and Sign up

--- app/src/main/java/com/example/bookingapproyaljourney/SplashScreenVer2.java
+++ app/src/main/java/com/example/bookingapproyaljourney/SplashScreenVer2.java
@@ -1,11 +1,14 @@
 package com.example.bookingapproyaljourney;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.viewpager.widget.ViewPager;
 
 import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.transition.Fade;
 import android.util.Pair;
 import android.view.View;
 import android.widget.ImageView;
@@ -18,17 +21,17 @@ public class SplashScreenVer2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen_ver2);
+        setTitle("");
+        ImageView imageView = findViewById(R.id.Logo);
+        Fade fade = new Fade();
+        View decor = getWindow().getDecorView();
 
-        Splash.blackIconStatusBar(SplashScreenVer2.this, R.color.light_Background);
-
-        Logo = findViewById(R.id.Logo);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent intent = new Intent(SplashScreenVer2.this, LoginActivity.class);
-                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SplashScreenVer2.this,
-                        Pair.create(Logo ,"logo"));
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SplashScreenVer2.this, imageView,ViewCompat.getTransitionName(imageView));
                 startActivity(intent, options.toBundle());
             }
         },3000);
--- app/src/main/java/com/example/bookingapproyaljourney/ui/activity/LoginActivity.java
+++ app/src/main/java/com/example/bookingapproyaljourney/ui/activity/LoginActivity.java
@@ -2,18 +2,28 @@ package com.example.bookingapproyaljourney.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.bookingapproyaljourney.R;
 import com.example.bookingapproyaljourney.Splash;
 
 public class LoginActivity extends AppCompatActivity {
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        textView = findViewById(R.id.Forgot);
 
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+            }
+        });
 
     }
 }
