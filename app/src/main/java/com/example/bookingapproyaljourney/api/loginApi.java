02e7fd9f8ea7fe32788ec2cbe361cc18e787ad6f
LongDuyDz <93507947+LongDuyDz@users.noreply.github.com>
2022-10-10 13:31:13 +0700

    UI detailproduct

---
+++ app/src/main/java/com/example/bookingapproyaljourney/api/loginApi.java
@@ -0,0 +1,12 @@
+package com.example.bookingapproyaljourney.api;
+
+import com.example.bookingapproyaljourney.model.user.User;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface loginApi {
+
+}
