43327718d75d61b8475593a6ade54ae6de459a4b
LongDuyDz <93507947+LongDuyDz@users.noreply.github.com>
2022-10-14 14:33:02 +0700

    check login

--- app/src/main/java/com/example/bookingapproyaljourney/ui/activity/LoginActivity.java
+++ app/src/main/java/com/example/bookingapproyaljourney/ui/activity/LoginActivity.java
@@ -2,19 +2,44 @@ package com.example.bookingapproyaljourney.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.bookingapproyaljourney.MainActivity;
 import com.example.bookingapproyaljourney.R;
 import com.example.bookingapproyaljourney.databinding.ActivityLoginBinding;
+import com.example.bookingapproyaljourney.model.user.UserLogin;
 import com.example.bookingapproyaljourney.view_model.LoginViewModel;
 
 public class LoginActivity extends AppCompatActivity {
+    private ConstraintLayout contentView;
+    private ImageView imageView;
+    private TextView textView;
+    private ImageView imageView2;
+    private EditText edEmail;
+    private EditText edPass;
+    private TextView tvForgotPass;
+    private AppCompatButton btnSignIn;
+    private TextView textView3;
+    private TextView tvSignUp;
+    private ProgressBar progressBar;
+    private String correct_email = "";
+    private String correct_password = "";
+
+    private UserLogin userLogin;
+
+
 
     private LoginViewModel loginViewModel;
 
@@ -25,6 +50,18 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        contentView = (ConstraintLayout) findViewById(R.id.contentView);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        textView = (TextView) findViewById(R.id.textView);
+        imageView2 = (ImageView) findViewById(R.id.imageView2);
+        edEmail = (EditText) findViewById(R.id.edEmail);
+        edPass = (EditText) findViewById(R.id.edPass);
+        tvForgotPass = (TextView) findViewById(R.id.tvForgotPass);
+        btnSignIn = (AppCompatButton) findViewById(R.id.btnSignIn);
+        textView3 = (TextView) findViewById(R.id.textView3);
+        tvSignUp = (TextView) findViewById(R.id.tvSignUp);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
 
         loginViewModel = new ViewModelProvider(this).get(LoginViewModel.class);
 
@@ -42,7 +79,16 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         binding.btnSignIn.setOnClickListener(v -> {
-            loginViewModel.login(binding.edEmail.getText().toString(), binding.edPass.getText().toString(), this.getResources().getString(R.string.LoginSuccess), this.getResources().getString(R.string.LoginFailed));
+            if (TextUtils.isEmpty(edEmail.getText().toString()) || TextUtils.isEmpty(edPass.getText().toString())){
+                Toast.makeText(LoginActivity.this,"Hãy điền đầy đủ thông tin",Toast.LENGTH_SHORT).show();
+            }else if (edEmail.getText().toString().equals(correct_email)){
+                if (edPass.getText().toString().equals(correct_password)){
+                    Toast.makeText(LoginActivity.this, "Mật khẩu không chính xác ",Toast.LENGTH_SHORT).show();
+                }
+            }else {
+                Toast.makeText(LoginActivity.this,"Mật khẩu hoặc tài khoản không đúng",Toast.LENGTH_SHORT).show();
+            }
+//            loginViewModel.login(binding.edEmail.getText().toString(), binding.edPass.getText().toString(), this.getResources().getString(R.string.LoginSuccess), this.getResources().getString(R.string.LoginFailed));
         });
 
         loginViewModel.getLoginResult().observe(this, new Observer<String>() {
