35cbb199b3a1c20a8223ecd5b428dd5f177947d2
minhdinh <minhdinhthanh@vccorp.vn>
2022-11-08 16:02:43 +0700

    ui comfirm mail

---
+++ app/src/main/java/com/example/bookingapproyaljourney/ui/activity/OtpActivity.java
@@ -0,0 +1,19 @@
+package com.example.bookingapproyaljourney.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.bookingapproyaljourney.databinding.ActivityOtpBinding;
+
+public class OtpActivity extends AppCompatActivity {
+
+    private ActivityOtpBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityOtpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
