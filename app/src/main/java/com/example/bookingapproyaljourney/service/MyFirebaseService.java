37560125054bd0fb1cec0755bf14899aaa85dce8
minhdinh <minhdinhthanh@vccorp.vn>
2022-11-25 10:54:40 +0700

    update noti icon

--- app/src/main/java/com/example/bookingapproyaljourney/service/MyFirebaseService.java
+++ app/src/main/java/com/example/bookingapproyaljourney/service/MyFirebaseService.java
@@ -17,6 +17,8 @@ import androidx.core.app.NotificationCompat;
 
 import com.example.bookingapproyaljourney.MainActivity;
 import com.example.bookingapproyaljourney.R;
+import com.example.bookingapproyaljourney.constants.AppConstant;
+import com.example.bookingapproyaljourney.ui.activity.StatusBillActivity;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -48,9 +50,11 @@ public class MyFirebaseService extends FirebaseMessagingService {
     }
 
     private void sendNotification(RemoteMessage remoteMessage) {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        Intent acceptIntent = createIntent();
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, acceptIntent, PendingIntent.FLAG_ONE_SHOT);
+
+        String name = (String) remoteMessage.getData().get("text");
 
         String channelId = getString(R.string.project_id);
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
@@ -61,6 +65,7 @@ public class MyFirebaseService extends FirebaseMessagingService {
                         .setColor(getResources().getColor(R.color.blue))
                         .setLargeIcon(getBitmapFromURL("https://scontent.fhan14-1.fna.fbcdn.net/v/t39.30808-6/301570226_821753485488801_359795053301422296_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=174925&_nc_ohc=P_yzIwHd75YAX-Lwhfs&_nc_ht=scontent.fhan14-1.fna&oh=00_AfAPWdELdPYV-2toWKTikvdLyAPtLsDbyVEVTm7HvVaSWA&oe=63842B84"))
                         .setContentTitle(remoteMessage.getNotification().getTitle())
+//                        .setContentTitle(name)
                         .setContentText(remoteMessage.getNotification().getBody())
                         .setAutoCancel(true)
                         .setSound(defaultSoundUri)
@@ -79,6 +84,7 @@ public class MyFirebaseService extends FirebaseMessagingService {
         }
         notificationManager.notify(0, notificationBuilder.build());
     }
+
     public static Bitmap getBitmapFromURL(String src) {
         try {
             URL url = new URL(src);
@@ -92,5 +98,12 @@ public class MyFirebaseService extends FirebaseMessagingService {
             // Log exception
             return null;
         }
+    }
+
+    private Intent createIntent() {
+        Intent intent = new Intent(this, StatusBillActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.putExtra(AppConstant.ID_ORDER, "RJ2019382");
+        return intent;
     }
 }
