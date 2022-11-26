d99b447354fe0002686d09aa529d62c05db8ed9c
BienLee <lengan2505@gmail.com>
2022-11-26 11:20:35 +0700

    select and edit img

--- app/src/main/java/com/example/bookingapproyaljourney/ui/activity/EditProfile.java
+++ app/src/main/java/com/example/bookingapproyaljourney/ui/activity/EditProfile.java
@@ -1,28 +1,35 @@
 package com.example.bookingapproyaljourney.ui.activity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
+import com.cloudinary.android.MediaManager;
+import com.cloudinary.android.callback.ErrorInfo;
+import com.cloudinary.android.callback.UploadCallback;
 import com.example.bookingapproyaljourney.R;
 import com.example.bookingapproyaljourney.constants.AppConstant;
 import com.example.bookingapproyaljourney.response.LoginResponse;
 import com.example.bookingapproyaljourney.view_model.LoginViewModel;
 import com.example.librarycireleimage.CircleImageView;
+import com.github.dhaval2404.imagepicker.ImagePicker;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class EditProfile extends AppCompatActivity {
     private MaterialToolbar toolBar;
@@ -38,6 +45,9 @@ public class EditProfile extends AppCompatActivity {
     private AppCompatButton saveEditProfile;
 
     private LoginViewModel loginViewModel;
+
+    private Uri imagePath;
+    private static final String TAG = "Upload ###";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,8 +76,56 @@ public class EditProfile extends AppCompatActivity {
         loginViewModel = new ViewModelProvider(this).get(LoginViewModel.class);
 
         initData();
+        initCongif();
+
+//        edit image
+        cameraEditProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImagePicker.with(EditProfile.this)
+                        .crop()
+                        .compress(1024)
+                        .maxResultSize(1080, 1080)
+                        .start(20);
+            }
+        });
+
+//        ban du lieu len clouldy
+        saveEditProfile.setOnClickListener(v -> {
+            Log.d(TAG, "button click");
+
+            MediaManager.get().upload(imagePath).callback(new UploadCallback() {
+
+                @Override
+                public void onStart(String requestId) {
+                    Log.d(TAG, "onStart " + "started");
+                }
+
+                @Override
+                public void onProgress(String requestId, long bytes, long totalBytes) {
+                    Log.d(TAG, "onStart " + imagePath);
+                }
+
+                // tra ve link img cloudy
+                @Override
+                public void onSuccess(String requestId, Map resultData) {
+//                    tvURL.setText(resultData.get("url").toString());
+                }
+
+                @Override
+                public void onError(String requestId, ErrorInfo error) {
+                    Log.d(TAG, "onStart " + error);
+                }
+
+                @Override
+                public void onReschedule(String requestId, ErrorInfo error) {
+                    Log.d(TAG, "onStart " + error);
+                }
+            }).dispatch();
+        });
     }
 
+// get data user
     private void initData() {
         SharedPreferences sharedPreferences = this.getSharedPreferences(AppConstant.SHAREDPREFERENCES_USER, Context.MODE_PRIVATE);
         String token = sharedPreferences.getString(AppConstant.TOKEN_USER, "");
@@ -83,4 +141,22 @@ public class EditProfile extends AppCompatActivity {
             }
         });
     }
+
+    //  set img cho circleAvt
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        imagePath = data.getData();
+        avtEditProfile.setImageURI(imagePath);
+    }
+
+    //  put dia chi clouldy
+    private void initCongif() {
+        Map config = new HashMap();
+        config.put("cloud_name", "dphlpcxrq");
+        config.put("api_key", "234574664596899");
+        config.put("api_secret", "qRHVXqdqyJHBvv4nhZWMQBv1oe0");
+        MediaManager.init(this, config);
+    }
+
 }
