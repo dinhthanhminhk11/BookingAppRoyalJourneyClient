138418587407e165f9e3b87451b9b15274b9a22b
baohoang201 <baohoang.tb.201@gmail.com>
2022-12-06 20:30:41 +0700

    UI change password

--- app/src/main/java/com/example/bookingapproyaljourney/ui/fragment/SettingFragment.java
+++ app/src/main/java/com/example/bookingapproyaljourney/ui/fragment/SettingFragment.java
@@ -1,16 +1,13 @@
 package com.example.bookingapproyaljourney.ui.fragment;
 
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.res.Configuration;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
@@ -20,13 +17,10 @@ import androidx.fragment.app.Fragment;
 
 import com.example.bookingapproyaljourney.MainActivity;
 import com.example.bookingapproyaljourney.R;
-import com.example.bookingapproyaljourney.constants.AppConstant;
 import com.example.bookingapproyaljourney.event.KeyEvent;
-import com.example.bookingapproyaljourney.utils.LanguageConfig;
+import com.example.bookingapproyaljourney.ui.activity.ChangePasswordActivity;
 
 import org.greenrobot.eventbus.EventBus;
-
-import java.util.Locale;
 
 
 public class SettingFragment extends Fragment {
@@ -39,12 +33,11 @@ public class SettingFragment extends Fragment {
 
 
     private Spinner spinnerLanguage;
-    public static final String[] languages = {"Choose","Tiếng Việt", "English"};
+    private ImageView imgChangePass;
+    private SwitchCompat compat;
+
+    public static final String[] languages = {"Choose Language", "Tiếng Việt", "English"};
     public int idLang = 10;
-
-    public SettingFragment() {
-    }
-
 
     public static SettingFragment newInstance(String param1, String param2) {
         SettingFragment fragment = new SettingFragment();
@@ -63,24 +56,28 @@ public class SettingFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_setting, container, false);
-
-
     }
 
-
-    SwitchCompat compat;
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         spinnerLanguage = view.findViewById(R.id.spinnerLanguage);
+        imgChangePass = view.findViewById(R.id.changePassSettings);
+
+        imgChangePass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ChangePasswordActivity.class);
+                startActivity(intent);
+            }
+        });
+
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, languages);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerLanguage.setAdapter(adapter);
@@ -106,11 +103,5 @@ public class SettingFragment extends Fragment {
 
             }
         });
-
-
     }
-
-
-
-
 }
