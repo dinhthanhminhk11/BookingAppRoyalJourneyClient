80b878a086261dcd77a4cf91f2af6195d2d97811
dinhminh <dinhthanhminhk11@gmail.com>
2022-12-26 04:30:48 -0800

    thay activity

--- app/src/main/java/com/example/bookingapproyaljourney/constants/AppConstant.java
+++ app/src/main/java/com/example/bookingapproyaljourney/constants/AppConstant.java
@@ -2,7 +2,7 @@ package com.example.bookingapproyaljourney.constants;
 
 public class AppConstant {
 
-    public static final String BASE = "https://7af3-123-25-30-77.ap.ngrok.io";
+    public static final String BASE = "http://192.168.0.100:8080";
     public static final String BASE_URL = BASE + "/api/";
     public static final String BASE_URL_CHAT = BASE;
     public static final String URL_API_GOOGLE_MAP = "https://maps.googleapis.com/";
